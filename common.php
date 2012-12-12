825ca4d04fba00b1fc9ad3075c5ac29468c8434d
Sazaju HITOKAGE <sazaju@gmail.com>
2012-12-12 23:23:33 +0100

    Review the size computing.

--- common.php
+++ common.php
@@ -396,41 +396,13 @@
 		}
 	}
 	
+	clearstatcache();
 	function getSize($path) {
 		if ($path === null) {
 			return 0;
-		}
-		else if (is_file($path)) {
-			// filesize will only return the lower 32 bits of
-			// the file's size! Make it unsigned.
-			$fmod = filesize($path);
-			$i = 0;
-			if ($fmod < 0) {
-			$fmod += 2.0 * (PHP_INT_MAX + 1);
-			
-			// find the upper 32 bits
-			
-			$myfile = fopen($path, "r");
-			
-			// feof has undefined behaviour for big files.
-			// after we hit the eof with fseek,
-			// fread may not be able to detect the eof,
-			// but it also can't read bytes, so use it as an
-			// indicator.
-			while (strlen(fread($myfile, 1)) === 1) {
-				fseek($myfile, PHP_INT_MAX, SEEK_CUR);
-				$i++;
-			}
-			
-			fclose($myfile);
-			
-			// $i is a multiplier for PHP_INT_MAX byte blocks.
-			// return to the last multiple of 4, as filesize has modulo of 4 GB (lower 32 bits)
-			if ($i % 2 == 1) $i--;
-			}
-			
-			// add the lower 32 bit to our PHP_INT_MAX multiplier
-			return ((float)($i) * (PHP_INT_MAX + 1)) + $fmod;
+		} else if (is_file($path)) {
+			$stat = stat($path);
+			return min($stat['size'], 512*$stat['blocks']);
 		}
 		else if (is_dir($path)) {
 			$size = 0;
